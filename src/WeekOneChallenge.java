f67ceec6df4dcedde85945a82e103435ab7c661a
jlepler <jlepler@montgomerycollege.edu>
2019-06-14 09:26:55 -0400

    Initial commit of the Weekly Challenge submission

---
+++ src/WeekOneChallenge.java
@@ -0,0 +1,21 @@
+/*This program will have the user input a number,
+ and the program will check whether or not the number is prime.
+  */
+
+import java.util.Scanner
+//Random randomNumber = new Random();   --- Will be used for option 2 of the challenge.
+
+public class WeekOneChallenge {
+    public static void main(String [] args){
+
+
+    Scanner inputNumber = new Scanner(System.in); //This line creates the variable for the user to input.
+
+
+
+
+
+
+    }
+
+}
